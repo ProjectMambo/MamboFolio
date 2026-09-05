cff737e0bfabb57b58fef1d6ab23437e81451e8f
KohKoh-Nut <kohkohnut1202@gmail.com>
2026-09-05 14:58:07 +0800

    chore(site): remove legacy implementation archive

--- archive/src/app/blog/the-beningging/page.tsx
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-import Page from "@/modules/Page";
-import { parseMarkdownFile } from "@/components/markdown/MarkdownParser";
-
-export default function GeneratedPage() {
-  const nodes = parseMarkdownFile(path.join(process.cwd(), "docs", "blog/The Beningging.md"));
-  return <Page nodes={nodes} />;
-}
--- archive/src/app/project/project-mambo/page.tsx
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-import Page from "@/modules/Page";
-import { parseMarkdownFile } from "@/components/markdown/MarkdownParser";
-
-export default function GeneratedPage() {
-  const nodes = parseMarkdownFile(path.join(process.cwd(), "docs", "project/Project Mambo.md"));
-  return <Page nodes={nodes} />;
-}
